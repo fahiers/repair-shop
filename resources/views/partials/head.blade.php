0dc0d1d0772802b1642096ac516810304f2cc36e
fahiers <fahieram@gmail.com>
2025-11-14 11:22:39 -0300

    ligth predeterminado

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,5 +10,12 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+<script>
+    // Establecer modo claro como predeterminado si no hay preferencia guardada
+    if (!window.localStorage.getItem('flux.appearance')) {
+        window.localStorage.setItem('flux.appearance', 'light');
+    }
+</script>
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
